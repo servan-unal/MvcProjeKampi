6e06a7863a6daa3ddf9aaae24972bd3ce00bc5e3
servan-unal <71839049+servan-unal@users.noreply.github.com>
2022-03-30 19:04:19 +0300

    Add project files.

---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            Context c = new Context();
+            var adminuserinfo = c.admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName &&
+            x.AdminPassword == p.AdminPassword);
+            if (adminuserinfo != null)
+            {
+                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName, false);
+                Session["AdminUserName"] = adminuserinfo.AdminUserName;
+                //yetkilendirme işlemleri yaptım. 
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
